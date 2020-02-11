d52377e67862d05c06499b9b4a9f053de2019141
boeriksson <bo.branzell@gmail.com>
2020-02-11 22:45:00 +0100

    tree work

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from '@emotion/styled'
+import styled, {css} from 'styled-components'
 
 const Container = styled.div`
     background-color: yellow;
@@ -10,6 +10,100 @@ const Container = styled.div`
     height: 100%;
 `
 
-export default () => (
-    <Container>Menu</Container>
-)
+const TreeTopUL = styled.ul`
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+`
+
+const Nested = styled.ul`
+    list-style-type: none;
+    display: block;
+`
+
+const Caret = styled.span`${({expanded}) => css`
+    cursor: pointer;
+    user-select: none;
+    
+    &::before {
+        content: "\\25B6";
+        color: black;
+        display: inline-block;
+        margin-right: 6px;
+    }
+    ${ !expanded && `
+        &::before {
+            transform: rotate(90deg);
+        }
+    `}
+`}`
+
+const payload = [
+    {
+        label: "lövträd",
+        expanded: true,
+        children: [
+            {
+                label: "björk",
+                expanded: false,
+                children: [
+                    {
+                        label: "skogsbjörk"
+                    },
+                    {
+                        label: "hängbjörk"
+                    }
+                ]
+            },
+            {
+                label: "ek",
+                expanded: true,
+                children: [
+                    {
+                        label: "skogsek"
+                    },
+                    {
+                        label: "rödek"
+                    },
+                    {
+                        label: "korkek"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: "barrträd",
+        expanded: false,
+        children: [
+            {
+                label: "tall"
+            },
+            {
+                label: "gran"
+            }
+        ]
+    }
+]
+
+
+export default () => {
+
+    const buildTree = (node, key) => {
+        return <li key={key}>
+            {node.label && node.children
+                ? <Caret expanded={node.expanded}>{node.label}</Caret>
+                : <span>{node.label}</span>}
+            {node.children && node.expanded &&
+            <Nested>{node.children.map((child, ix) => buildTree(child, ix))}</Nested>}
+        </li>
+    }
+
+    return (
+        <Container>
+            <TreeTopUL>
+                {buildTree({children: payload, expanded: true}, 'root')}
+            </TreeTopUL>
+        </Container>
+    )
+}
